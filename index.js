dcdb11cf457554a3624a67d79c3202f77ab3f62f
Muztahid Durjoy <43501336+muztahiddurjoy@users.noreply.github.com>
2021-12-09 15:57:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   axios.get('https://galibotcord-default-rtdb.firebaseio.com/galis.json').then((res)=>{
     galis = Object.values(res.data)
+    console.log(galis)
   }).catch((e)=>{
     console.log(e)
   })
@@ -17,10 +18,26 @@ client.once('ready', () => {
 client.on('messageCreate', message => {
     const t = message.content;
   if(t.includes('%gali')){
-    message.reply(`${t.substr(5,t.length)} ${galis[Math.floor(Math.random() * (galis.length - 0))].gali}`)
+    let target = t.substr(5,t.length)
+    if(target.toLowerCase().includes("durjoy") || target.toLowerCase().includes("botjoy") || target.toLowerCase().includes('<@!917045708203380766>') || target.toLowerCase().includes('<@!544406696563572739>')){
+     message.reply(`Hopp beda\n<@!${message.author.id}> ${galis[Math.floor(Math.random() * (galis.length - 0))].gali}`) 
+    }
+    else{
+      message.reply(`${target} ${galis[Math.floor(Math.random() * (galis.length - 0))].gali}`)
+    }
   }
   else if(t.includes('%welcome')){
     message.reply(`Welcome lodu ${t.substr(8,t.length)}`)
+  }
+  else if(t.includes('%tornado')){
+    if(message.author.id == '544406696563572739'){
+      galis.map((v,i)=>{
+        message.reply(`${t.substr(8,t.length)} ${v.gali}`)
+      })
+    }
+    else{
+      message.reply('shob command shobar jonno na baba')
+    }
   }
   else if(t.includes('%addgali')){
     axios.put(`https://galibotcord-default-rtdb.firebaseio.com/galis/${new Date().getTime()}.json`,{gali:t.substr(8,t.length),author: message.author.username}).then(()=>{
@@ -28,20 +45,8 @@ client.on('messageCreate', message => {
     }).catch((e)=>{
       message.reply(e)
     })
-    axios.get('https://galibotcord-default-rtdb.firebaseio.com/galis.json').then((res)=>{
-    galis = Object.values(res.data)
-    }).catch((e)=>{
-      console.log(e)
-    })
   }
- /* else if(t.includes("")){
 
-  }*/
 });
-client.on('messageDelete', (s,d)=>{
-//  console.log(client)
-  //s.reply(`${s.author.username} Message Delete korle gay!`)
-  
-})
 
 client.login(process.env.BOT_TOKEN);
